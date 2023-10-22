b7ed6ed00c73c2a4779cb6a580bda832d85470f4
Anze <2290434225@qq.com>
2023-10-22 16:51:34 +0800

    weifuwu1

---
+++ cart-service/src/main/java/com/hmall/cart/service/impl/CartServiceImpl.java
@@ -0,0 +1,145 @@
+package com.hmall.cart.service.impl;
+
+import cn.hutool.core.collection.CollUtil;
+import cn.hutool.core.util.RandomUtil;
+import cn.hutool.core.util.StrUtil;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmall.common.exception.BizIllegalException;
+import com.hmall.common.utils.BeanUtils;
+import com.hmall.common.utils.CollUtils;
+import com.hmall.common.utils.UserContext;
+import com.hmall.cart.domain.dto.CartFormDTO;
+import com.hmall.cart.domain.dto.ItemDTO;
+import com.hmall.cart.domain.po.Cart;
+import com.hmall.cart.domain.vo.CartVO;
+import com.hmall.cart.mapper.CartMapper;
+import com.hmall.cart.service.ICartService;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.Resource;
+import java.net.URI;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+/**
+ * <p>
+ * 订单详情表 服务实现类
+ * </p>
+ *
+ * @author 虎哥
+ * @since 2023-05-05
+ */
+@Service
+@RequiredArgsConstructor //只会给有final修饰的变量加构造函数
+public class CartServiceImpl extends ServiceImpl<CartMapper, Cart> implements ICartService {
+    private final RestTemplate restTemplate;
+
+    private final DiscoveryClient discoveryClient;
+
+    @Override
+    public void addItem2Cart(CartFormDTO cartFormDTO) {
+
+    }
+
+    @Override
+    public List<CartVO> queryMyCarts() {
+        // 1.查询我的购物车列表
+        List<Cart> carts = lambdaQuery().eq(Cart::getUserId, 1L/*UserContext.getUser()*/).list();
+        if (CollUtils.isEmpty(carts)) {
+            return CollUtils.emptyList();
+        }
+
+        // 2.转换VO
+        List<CartVO> vos = BeanUtils.copyList(carts, CartVO.class);
+
+        // 3.处理VO中的商品信息
+        handleCartItems(vos);
+
+        // 4.返回
+        return vos;
+    }
+
+    private void handleCartItems(List<CartVO> vos) {
+        // 1.获取商品id
+        Set<Long> itemIds = vos.stream().map(CartVO::getItemId).collect(Collectors.toSet());
+        // 2.查询商品
+        //2.1根据服务名称获取服务的实例列表
+        List<ServiceInstance> instances = discoveryClient.getInstances("item-service");
+        if(CollUtil.isEmpty(instances)){
+            return;
+        }
+        //负载均衡
+        ServiceInstance instance = instances.get(RandomUtil.randomInt(instances.size()));
+        URI uri = instance.getUri();
+        //2.1 利用RestTmplate发送http请求，得到http响应
+        ResponseEntity<List<ItemDTO>> response = restTemplate.exchange(
+                uri+"/items?ids={ids}",
+                HttpMethod.GET,
+                null,
+                new ParameterizedTypeReference<List<ItemDTO>>() {
+                },
+                Map.of("ids", CollUtil.join(itemIds, ","))//url中{ids}的值,CollUtil是hutool包的将集合通过，拼接成字符串
+        );
+        //2.2解析响应
+        if(!response.getStatusCode().is2xxSuccessful()){
+            //查询失败,直接结束
+            return;
+        }
+        List<ItemDTO> items = response.getBody();
+        if (CollUtils.isEmpty(items)) {
+            return;
+        }
+        // 3.转为 id 到 item的map
+        Map<Long, ItemDTO> itemMap = items.stream().collect(Collectors.toMap(ItemDTO::getId, Function.identity()));
+        // 4.写入vo
+        for (CartVO v : vos) {
+            ItemDTO item = itemMap.get(v.getItemId());
+            if (item == null) {
+                continue;
+            }
+            v.setNewPrice(item.getPrice());
+            v.setStatus(item.getStatus());
+            v.setStock(item.getStock());
+        }
+    }
+
+    @Override
+    public void removeByItemIds(Collection<Long> itemIds) {
+        // 1.构建删除条件，userId和itemId
+        QueryWrapper<Cart> queryWrapper = new QueryWrapper<Cart>();
+        queryWrapper.lambda()
+                .eq(Cart::getUserId, UserContext.getUser())
+                .in(Cart::getItemId, itemIds);
+        // 2.删除
+        remove(queryWrapper);
+    }
+
+    private void checkCartsFull(Long userId) {
+        int count = Math.toIntExact(lambdaQuery().eq(Cart::getUserId, userId).count());
+        if (count >= 10) {
+            throw new BizIllegalException(StrUtil.format("用户购物车课程不能超过{}", 10));
+        }
+    }
+
+    private boolean checkItemExists(Long itemId, Long userId) {
+        int count = Math.toIntExact(lambdaQuery()
+                .eq(Cart::getUserId, userId)
+                .eq(Cart::getItemId, itemId)
+                .count());
+        return count > 0;
+    }
+}

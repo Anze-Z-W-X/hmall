4b06f65c80188d02d591b08bb72c70541cb0480e
Anze <2290434225@qq.com>
2023-11-13 15:35:22 +0800

    weifuwu5

--- hm-common/src/main/java/com/hmall/common/interceptor/UserInfoInterceptor.java
+++ hm-common/src/main/java/com/hmall/common/interceptor/UserInfoInterceptor.java
@@ -12,7 +12,7 @@ public class UserInfoInterceptor implements HandlerInterceptor {
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         //1.获取请求头中的用户信息
         String userInfo = request.getHeader("user-info");
-        //2.判断是否为空，不为空，存入UserContext
+        //2.判断是否为空，如果不为空，存入UserContext
         if(StrUtil.isNotBlank(userInfo)){
             UserContext.setUser(Long.valueOf(userInfo));
         }
